f28b30638589cc9fbb6d44ac06dd16753cd0ca9b
Sebastien Valat <sebastien.valat.dev@orange.fr>
2012-11-29 22:50:36 +0100

    * add statm analysis

---
+++ modules/statm/statm.c
@@ -0,0 +1,34 @@
+/*****************************************************
+             PROJECT  : htopml
+             VERSION  : 0.1.0-dev
+             DATE     : 11/2012
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+/********************  HEADERS  *********************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "statm.h"
+
+/*********************  CONSTS  *********************/
+static const char * CST_STATM_FILE = "/proc/self/statm";
+
+/*******************  FUNCTION  *********************/
+void statm_load(struct statm* stat)
+{
+	int cnt;
+	FILE * fp = fopen(CST_STATM_FILE,"r");
+	if (fp == NULL)
+	{
+		perror(CST_STATM_FILE);
+		abort();
+	}
+
+	cnt = fscanf(fp,"%lu %lu %lu %lu %lu %lu %lu\n",
+	       &stat->size,&stat->resident, &stat->share, &stat->text, &stat->lib, &stat->data, &stat->dirty);
+	assert(cnt == 7);
+
+	fclose(fp);
+}
---
+++ modules/statm/statm.h
@@ -0,0 +1,39 @@
+/*****************************************************
+             PROJECT  : htopml
+             VERSION  : 0.1.0-dev
+             DATE     : 11/2012
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+#ifndef HTOPML_LINUX_PROC_STATM_H
+#define HTOPML_LINUX_PROC_STATM_H
+
+/********************  HEADERS  *********************/
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*********************  STRUCT  *********************/
+struct statm
+{
+	size_t size;
+	size_t resident;
+	size_t share;
+	size_t text;
+	size_t lib;
+	size_t data;
+	size_t dirty;
+	
+};
+
+/*******************  FUNCTION  *********************/
+void statm_load(struct statm * stat);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
