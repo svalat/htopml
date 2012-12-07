604f4b170fe48b34ac92afaf7ff76a5b4ae70cf0
Sebastien Valat <sebastien.valat.dev@orange.fr>
2012-12-07 22:38:39 +0100

    * Move the url to table JS code in common to reuse it.

--- modules/environ/environ-graphs.js
+++ modules/environ/environ-graphs.js
@@ -1,36 +1,3 @@
-function requestData() {
-	$.ajax({
-		url: 'environ.json',
-		contentType: 'application/json; charset=utf-8',
-		success: function(point) {
-			var table = document.getElementById('vartable');
-			var content = "";
-			for (v in point)
-			{
-				var tr = document.createElement('tr');
-				var tdName = document.createElement('td');
-				var pre = document.createElement('pre');
-				pre.innerHTML = v;
-				tdName.appendChild(pre);
-				var tdValue = document.createElement('td');
-				//tdValue.style.setProperty("word-wrap","break-word");
-				pre = document.createElement('pre');
-				pre.innerHTML = point[v];
-				tdValue.appendChild(pre);
-				tr.appendChild(tdName);
-				tr.appendChild(tdValue);
-				table.appendChild(tr);
-			}
-			
-		},
-		type: "GET",
-//         async: false,
-		error:function (xhr, status, error){
-		alert("Erreur de chargement du fichier '"+self.url+"' : "+xhr.responseText+" ("+status+" - "+error+")");
-		},
-	});
-}
-
 $(document).ready(function() {
-	requestData();
+	htopmlFillVarArrayFromUrl('vartable','environ.json');
 });
--- src/www/common.js
+++ src/www/common.js
@@ -34,3 +34,42 @@ function htopmlUpdateGraphs() {
 	}
 	setTimeout(htopmlUpdateGraphs, graphUpdateTime);
 }
+
+/* Function to quickly fill an array with a key => value representation */
+function htopmlFillVarArray(tableId,data)
+{
+	var table = document.getElementById(tableId);
+	var content = "";
+	for (v in data)
+	{
+		var tr = document.createElement('tr');
+		var tdName = document.createElement('td');
+		var pre = document.createElement('pre');
+		pre.innerHTML = v;
+		tdName.appendChild(pre);
+		var tdValue = document.createElement('td');
+		//tdValue.style.setProperty("word-wrap","break-word");
+		pre = document.createElement('pre');
+		pre.innerHTML = data[v];
+		tdValue.appendChild(pre);
+		tr.appendChild(tdName);
+		tr.appendChild(tdValue);
+		table.appendChild(tr);
+	}
+}
+
+/* Load a table value (with key => value format) from a fiven URL and fill an html table with it. */
+function htopmlFillVarArrayFromUrl(tableId,updateUrl) {
+	$.ajax({
+		url: updateUrl,
+		contentType: 'application/json; charset=utf-8',
+		success: function(point) {
+			htopmlFillVarArray(tableId,point);
+		},
+		type: "GET",
+		//async: false,
+		//error:function (xhr, status, error){
+		//alert("Erreur de chargement du fichier '"+self.url+"' : "+xhr.responseText+" ("+status+" - "+error+")");
+		//},
+	});
+}
