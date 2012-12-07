ff3424a344c7c7fdb43bd8ce0b4db1f66067b34f
Sebastien Valat <sebastien.valat.dev@orange.fr>
2012-12-07 20:28:43 +0100

    * Some cleanup in generation of cmake command in configure  * Also add support of option for force cmake rpaths (seems not working, need check)

--- cmake/user-options.cmake
+++ cmake/user-options.cmake
@@ -14,30 +14,31 @@ set(ENABLE_VALGRIND no CACHE BOOL "Wrap all test execution with valgrind memchec
 set(ENABLE_GCC_COVERAGE no CACHE BOOL "Compile htopml library with coverage GCC CFLAGS.")
 set(DISABLE_TESTS no CACHE BOOL "Disable compilation of svUnitTest tests.")
 set(ENABLE_LOCAL_PATHS no CACHE BOOL "Setup absolute path to run in build directory, not for installation.")
+set(ENABLE_RPATH no CACHE BOOL "Force to use rpath for all libs when installing.")
 
 ######################################################
 #Take care of lib suffix for lib64 directory on some distributions
 if (NOT LIB_SUFFIX)
-        set(LIB_SUFFIX "")
+	set(LIB_SUFFIX "")
 endif (NOT LIB_SUFFIX)
 message(STATUS "LIB_SUFFIX : ${LIB_SUFFIX}")
 
 ######################################################
 #Display info on current configuration
 macro (print_variable_status var_name)
-        if (${var_name})
-                message(STATUS "${var_name} : yes")
-        else (${var_value})
-                message(STATUS "${var_name} : no")
-        endif (${var_name})
+	if (${var_name})
+		message(STATUS "${var_name} : yes")
+	else (${var_value})
+		message(STATUS "${var_name} : no")
+	endif (${var_name})
 endmacro (print_variable_status)
 
 ######################################################
 #enable coverage profiling
 IF (ENABLE_GCC_COVERAGE)
-        set(CMAKE_CXX_FLAGS "-O0 -fprofile-arcs -ftest-coverage")
-        set(CMAKE_C_FLAGS "-O0 -fprofile-arcs -ftest-coverage")
-        set(CMAKE_EXE_LINKER_FLAGS "-fprofile-arcs -ftest-coverage")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -fprofile-arcs -ftest-coverage")
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 -fprofile-arcs -ftest-coverage")
+	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-arcs -ftest-coverage")
 ENDIF (ENABLE_GCC_COVERAGE)
 
 ######################################################
@@ -48,3 +49,9 @@ ELSE (ENABLE_LOCAL_PATHS)
 	add_definitions(-DHTOPML_EXTDEPS_PATH="${CMAKE_INSTALL_PREFIX}/share/htopml/www")
 	add_definitions(-DHTOPML_WWW_PATH="${CMAKE_INSTALL_PREFIX}/share/htopml/www")
 ENDIF (ENABLE_LOCAL_PATHS)
+
+######################################################
+IF (ENABLE_RPATH)
+	SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+	SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+ENDIF(ENABLE_RPATH)
